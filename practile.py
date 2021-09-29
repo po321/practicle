4107a88c064fdb37586c3453fadddd14003135fe
po321 <91618657+po321@users.noreply.github.com>
2021-09-29 14:59:14 +0000

    Add files via upload

---
+++ practile.py
@@ -0,0 +1,81 @@
+#program made Hacker
+
+
+
+l=int(input("Length:",))
+
+if True:
+    l==float
+else:
+    l==int
+
+
+b=int(input("Bredth:",))
+
+if True:
+    b==int
+else:
+    b==float
+
+h=int(input("Height:",))
+
+if True:
+    h==int
+else:
+    h==float
+
+
+r=int(input("Radius:",))
+
+if True:
+    r==int
+else:
+    r==float
+
+s=int(input("Side:",))
+
+if True:
+    s==int
+else:
+    s==float
+
+
+
+
+
+area_of_triangle=1/2*b*h
+area_of_rectangle=l*b
+area_of_circle=3.14*r*r
+area_of_square=s*s
+
+print("area of triangle press 1")
+print("area of rectangle press 2")
+print("area of circle press 3")
+print("area of square press 4")
+
+your_choice=int(input("Enter Your choice:",))
+
+
+if True:
+    your_choice!=1,2,3,4
+else:
+    print(your_choice)
+
+
+
+if your_choice==1:(
+    print(area_of_triangle)
+)
+
+
+if your_choice==2:(
+    print(area_of_rectangle)
+)
+
+if your_choice==3:(
+    print(area_of_circle)
+)
+
+if your_choice==4:(
+    print(area_of_square)
+)
